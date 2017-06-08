5bccde65c5cae629b9ad47ac9feec16c14b50e7f
jalbee <jesse@truex.com>
2017-06-08 16:02:34 -0700

    added fonts

--- public/js/multiplayer-quiz.js
+++ public/js/multiplayer-quiz.js
@@ -1,25 +1,78 @@
-      var socket = io();
-      var n = nunchuck.init('host', socket);
-      var players = {};
-      var numPlayers = 1234;
 
-      n.onJoin(function(data){
 
-        if (!players[data.username]){
-          players[data.username] = numPlayers;
-          numPlayers+=1;
-          createPlane(players[data.username]);
-          $('.users').append("<br><strong>" + data.username + "</strong>")
-        }
+var init = function() {
+  createRoom();
+  showIntro();
+};
 
-        n.receive(function(data){
-          if (players[data.username]){
-            data.id = players[data.username];
-            movePlane(data)
-          }
-        })
-      });
 
-      $(document).ready(function(){
-        $('.code').text(n.roomId)
-      });
+
+
+
+
+
+
+
+
+var showIntro = function() {
+  slideUp($('#instructions'), 500);
+}
+
+var showQuizBackground = function() {
+  $("#bg-splash").removeClass('hidden');
+
+}
+
+
+
+
+
+
+
+
+
+
+var slideUp = function($element, delay) {
+  if (delay > 0) {
+    setTimeout(slideUp, delay, $element, 0);
+    return;
+  }
+
+  $element.addClass('animated slideInUp').removeClass('hidden');
+}
+
+var createRoom = function() {
+  var socket = io();
+  var n = nunchuck.init('host', socket);
+  var players = {};
+  var numPlayers = 1234;
+
+  n.onJoin(function(data){
+
+    if (!players[data.username]){
+      players[data.username] = numPlayers;
+      numPlayers+=1;
+      createPlane(players[data.username]);
+      $('.users').append("<br><strong>" + data.username + "</strong>")
+    }
+
+    n.receive(function(data){
+      if (players[data.username]){
+        data.id = players[data.username];
+        movePlane(data)
+      }
+    })
+  });
+};
+
+
+
+$(document).ready(function(){
+  init();
+});
+
+
+
+
+
+
